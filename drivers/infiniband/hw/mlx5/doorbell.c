efd43205b16c3ca716c8c806d3410a96f441b2fb
Davide Rossetti <davide.rossetti@gmail.com>
2017-01-23 17:24:26 -0800

    add peer memory support to mlx5_ib_db_map_user

--- drivers/infiniband/hw/mlx5/doorbell.c
+++ drivers/infiniband/hw/mlx5/doorbell.c
@@ -41,7 +41,41 @@ struct mlx5_ib_user_db_page {
 	struct ib_umem	       *umem;
 	unsigned long		user_virt;
 	int			refcnt;
+
+	atomic_t                invalidated;
+	struct completion       invalidation_comp;
 };
+
+static void mlx5_db_invalidate_umem(void *invalidation_cookie,
+                                    struct ib_umem *umem,
+                                    unsigned long addr, size_t size)
+{
+
+	struct mlx5_ib_user_db_page *page = (struct mlx5_ib_user_db_page *)invalidation_cookie;
+
+	printk(KERN_INFO "%s page=%p umem=%p\n", __FUNCTION__, page, umem);
+
+	/* This function is called under client peer lock so its resources are race protected */
+	if (atomic_inc_return(&page->invalidated) > 1) {
+		printk(KERN_ERR "%s invalidation is already in-flight\n", __FUNCTION__);
+		umem->invalidation_ctx->inflight_invalidation = 1;
+		return;
+	}
+
+        if (umem != page->umem) {
+                printk(KERN_ERR "%s error unexpected cq->uem=%p != umem=%p\n", __FUNCTION__, page->umem, umem);
+        } else {
+                page->umem = NULL;
+        }
+
+	umem->invalidation_ctx->peer_callback = 1;
+	// TODO:
+	// - free MTTs related, etc..
+	// - make it ODP friendly ?
+	printk(KERN_ERR "releasing umem=%p\n", umem);
+	ib_umem_release(umem);
+	complete(&page->invalidation_comp);
+}
 
 int mlx5_ib_db_map_user(struct mlx5_ib_ucontext *context, unsigned long virt,
 			struct mlx5_db *db)
@@ -49,11 +83,17 @@ int mlx5_ib_db_map_user(struct mlx5_ib_ucontext *context, unsigned long virt,
 	struct mlx5_ib_user_db_page *page;
 	int err = 0;
 
+	printk(KERN_INFO "%s %d\n", __FUNCTION__, __LINE__);
+
 	mutex_lock(&context->db_page_mutex);
 
 	list_for_each_entry(page, &context->db_page_list, list)
-		if (page->user_virt == (virt & PAGE_MASK))
-			goto found;
+		if (page->user_virt == (virt & PAGE_MASK)) {
+                        if (!page->umem)
+                                printk(KERN_INFO "INFO %s skipping page=%p as umem==NULL\n", __FUNCTION__, page);
+                        else
+                                goto found;
+                }
 
 	page = kmalloc(sizeof(*page), GFP_KERNEL);
 	if (!page) {
@@ -63,21 +103,32 @@ int mlx5_ib_db_map_user(struct mlx5_ib_ucontext *context, unsigned long virt,
 
 	page->user_virt = (virt & PAGE_MASK);
 	page->refcnt    = 0;
-	page->umem      = ib_umem_get(&context->ibucontext, virt & PAGE_MASK,
-				      PAGE_SIZE, 0, 0);
+	//page->umem      = ib_umem_get(&context->ibucontext, virt & PAGE_MASK,
+	//			      PAGE_SIZE, 0, 0);
+	page->umem      = ib_umem_get_ex(&context->ibucontext, virt & PAGE_MASK,
+                                         PAGE_SIZE, 0, 0, 1);
 	if (IS_ERR(page->umem)) {
 		err = PTR_ERR(page->umem);
+                printk(KERN_ERR "ERR %s error %d in ib_umem_get_ex virt=%lx\n", __FUNCTION__, err, page->user_virt);
 		kfree(page);
 		goto out;
 	}
 
 	list_add(&page->list, &context->db_page_list);
 
+	atomic_set(&page->invalidated, 0);
+	if (page->umem && page->umem->ib_peer_mem) {
+                printk(KERN_INFO "%s got peer_mem, umem=%p\n", __FUNCTION__, page->umem);
+		init_completion(&page->invalidation_comp);
+		ib_umem_activate_invalidation_notifier(page->umem, mlx5_db_invalidate_umem, page);
+	}
+
 found:
 	db->dma = sg_dma_address(page->umem->sg_head.sgl) + (virt & ~PAGE_MASK);
 	db->u.user_page = page;
 	++page->refcnt;
 
+	printk(KERN_INFO "%s %d dma=%lx\n", __FUNCTION__, __LINE__, (unsigned long)db->dma);
 out:
 	mutex_unlock(&context->db_page_mutex);
 
@@ -90,7 +141,10 @@ void mlx5_ib_db_unmap_user(struct mlx5_ib_ucontext *context, struct mlx5_db *db)
 
 	if (!--db->u.user_page->refcnt) {
 		list_del(&db->u.user_page->list);
-		ib_umem_release(db->u.user_page->umem);
+                if (db->u.user_page->umem)
+                    ib_umem_release(db->u.user_page->umem);
+                else
+                    printk(KERN_INFO "%s %d umem already released\n", __FUNCTION__, __LINE__);
 		kfree(db->u.user_page);
 	}
 
